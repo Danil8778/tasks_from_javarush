b94506b83e4572dddbabc4e9676fc190ca31cde6
Danil Pnevsky <dpnevsky@gmail.com>
2023-09-21 22:08:52 +0300

    add tasks

---
+++ src/main/java/com/pnevsky/string/Tokenizer.java
@@ -0,0 +1,22 @@
+package com.pnevsky.string;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Tokenizer {
+    public static void main(String[] args) {
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer stringTokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[stringTokenizer.countTokens()];
+        int i = 0;
+        while (stringTokenizer.hasMoreTokens()) {
+            result[i++] = stringTokenizer.nextToken();
+        }
+        return result;
+    }
+}
